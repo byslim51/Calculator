05a9949fb7a8e4220b05722630eec5463fb19560
Slim <you@example.com>
2024-06-26 16:49:59 +0500

    Refactoring Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,25 @@ public class Main {
         System.out.print("Введите действие (* , / , + , -):");
         String trash = scan.nextLine();
         String action2 = scan.nextLine();
-        switch (action2){
+        switch (action2) {
 
             case "+":
-                int resAdd = Add(num1 , num2);
+                int resAdd = add(num1, num2);
                 printNum(resAdd);
                 break;
 
             case "-":
-                int resMinus = Minus(num1 , num2);
+                int resMinus = minus(num1, num2);
                 printNum(resMinus);
                 break;
 
             case "*":
-                int resMultiply =Multiply(num1 , num2);
+                int resMultiply = multiply(num1, num2);
                 printNum(resMultiply);
                 break;
 
             case "/":
-                int resDivide = Divide(num1, num2);
+                int resDivide = divide(num1, num2);
                 printNum(resDivide);
                 break;
 
@@ -36,28 +36,33 @@ public class Main {
                 System.out.println("Вы ввели что то неправильно");
         }
     }
-    public static int Add (int num1 , int num2){
+
+    public static int add(int num1, int num2) {
         int result = (num1 + num2);
-        return  result;
+        return result;
 
     }
-    public static int Minus (int num1 , int num2){
+
+    public static int minus(int num1, int num2) {
         int result = (num1 - num2);
         return result;
     }
-    public static int Multiply (int num1 , int num2){
+
+    public static int multiply(int num1, int num2) {
         int result = (num1 * num2);
         return result;
     }
-    public static int Divide (int num1 , int num2){
-        if (num2 == 0){
+
+    public static int divide(int num1, int num2) {
+        if (num2 == 0) {
             System.out.println("Вы ввели недопустимое число");
             return 0;
         }
         int result = (num1 / num2);
         return result;
     }
-    public static void printNum(int result){
+
+    public static void printNum(int result) {
         System.out.println("Итог: " + result);
     }
 
