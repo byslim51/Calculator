8040e3cae1c2c0e613dbec33fde5055aab7547b8
Slim <you@example.com>
2024-06-30 00:23:02 +0500

    BinaryArray

---
+++ src/Array/BinaryArray.java
@@ -0,0 +1,50 @@
+package Array;
+
+public class BinaryArray {
+    public static void main(String[] args) {
+        chessBoardTest();
+    }
+
+    public static void chessBoard() {
+        String[][] arr2 = new String[8][8];
+        for (int i = 0; i < arr2.length; i++) {
+            for (int i2 = 0; i2 < arr2[i].length; i2++) {
+                if (i2 % 2 == 0) {
+                    arr2[i][i2] = "W ";
+                } else {
+                    arr2[i][i2] = "B ";
+                }
+            }
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            for (int i2 = 0; i2 < arr2[i].length; i2++) {
+                System.out.print(arr2[i][i2]);
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void chessBoardTest() {
+        String[][] arr2 = new String[8][8];
+        for (int i = 0; i < arr2.length; i++) {
+            for (int i2 = 0; i2 < arr2[i].length; i2++) {
+                if (i2 % 2 == 0) {
+                    arr2[i][i2] = "W ";
+                } else {
+                    arr2[i][i2] = "B ";
+                }
+            }
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            for (int i2 = 0; i2 < arr2[i].length; i2++) {
+                System.out.print(arr2[i][i2]);
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
