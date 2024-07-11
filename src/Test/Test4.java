109e9cc9a3376d68dc87c2aa9012c7ac56b9eb77
Slim <you@example.com>
2024-07-11 14:29:02 +0500

    Рекурсивный код

--- src/Test/Test4.java
+++ src/Test/Test4.java
@@ -1,6 +1,7 @@
 package Test;
 
 public class Test4 {
+// С рекурсией:
     public static int square(int x1, int x2) {
         if (x2 == 0) {
             return 1;
@@ -15,22 +16,27 @@ public class Test4 {
         int b = 3;
         System.out.println(square(a, b));
     }
-}
+
 
 // Без рекурсий:
+ public static int square2(int y1, int y2) {
+     int result = 0;
+     while (y2 > 0) {
+         result *= y1;
+         y2--;
+     }
+     return result;
+ }
 
-// public static int square2(int y1, int y2) {
-//     int result = 1;
-//     while (y2 > 0) {
-//         result *= y1;
-//         y2--;
-//     }
-//     return result;
-// }
-//
-// public static void main(String[] args) {
-//     int a = 4;
-//     int b = 3;
-//     System.out.println(square2(a, b));
-// }
-//  }
+
+// Без рекурсий но с циклом:
+    public static int square3 (int y1, int y2){
+        int result = 1;
+
+        for (int i = 0; i < y2; i++) {
+            result *= y1;
+        }
+        return result;
+    }
+  }
+
