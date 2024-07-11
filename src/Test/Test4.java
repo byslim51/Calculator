d07eaaf9cfa8644b88968dfabd57e25900ba677c
Slim <you@example.com>
2024-07-11 13:41:36 +0500

    Рекурсивный код

--- src/Test/Test4.java
+++ src/Test/Test4.java
@@ -1,19 +1,20 @@
 package Test;
 
 public class Test4 {
-    public static int count(int a) {
-        int x = 0;
-        if (a > 0) {
-            System.out.println(a);
-            x = count(a - 1);
-        }
-        return x;
-    }
+  public static int square (int x1, int x2){
+      if (x2 == 0){
+          return 1;
+      } else {
+          return x1 * square(x1, x2 - 1);
+      }
+
+  }
 
     public static void main(String[] args) {
-
-        System.out.println(count(5));
+        int a = 4;
+        int b = 3;
+        System.out.println(square(a,b));
     }
-    }
+}
 
 
